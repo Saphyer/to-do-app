15949e3d5037ee288902c7d33c7cd9aab33e2424
Saphyer <saphyer@me.com>
2019-01-15 09:23:19 -0500

    assignment-dom-scripting-event1

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /*jshint esversion: 6 */
 
-function onReady(){
-  const ADD_TODO_FORM = document.getElementById("addToDoForm");
-  const NEW_TODO_TEXT = document.getElementById("newToDoText");
-  const TODO_LIST = document.getElementById("toDoList");
+function onReady() {
+  const addToDoForm = document.getElementById("addToDoForm");
+  const newToDoText = document.getElementById("newToDoText");
+  const toDoList = document.getElementById("toDoList");
 
-  ADD_TODO_FORM.addEventListner("submit", (event) => {
+  addToDoForm.addEventListner("submit", event => {
     event.preventDefault();
 
-    let title = NEW_TODO_TEXT.value; //gets text
+    let title = newToDoText.value; //gets text
 
     let newLi = document.createElement("li"); //<li></li>
     let checkbox = document.createElement("input");
@@ -17,20 +17,19 @@ function onReady(){
     let deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
 
-    deleteBtn.addEventListner("click", function(event){
+    deleteBtn.addEventListner("click", function event
       // console.log(event);
       // this.parentElement represents the button's <li> parent
-      TODO_LIST.removeChild(this.parentElement);
-    });
+      toDoList.removeChild(this.parentElement);
 
     newLi.textContent = title; // set the title
     newLi.appendChild(checkbox);
     newLi.appendChild(deleteBtn);
-    TODO_LIST.appendChild(newLi);
-    NEW_TODO_TEXT.value = "";
-  });
-}
+    toDoList.appendChild(newLi);
+    newToDoText.value = "";
 
 window.onload = function() {
   onReady();
- };
+};
+});
+}
