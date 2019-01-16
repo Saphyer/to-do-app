6ac55b733f33c660d32a52ae077b0d305b054690
Saphyer <saphyer@me.com>
2019-01-16 13:30:37 -0500

    assignment-dom-scripting-event2: completed

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function onReady() {
       const deleteBtn = document.createElement("button");
       deleteBtn.textContent = "Delete";
 
-      deleteBtn.addEventListener("click", event => {
+      deleteBtn.addEventListener("click", () => {
        toDos = toDos.filter(function(item){
         return item.id !== toDo.id;
        });
